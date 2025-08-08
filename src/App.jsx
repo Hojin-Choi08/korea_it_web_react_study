4fb40d6cae3bb6340259f159f1f36cad12a1c95b
Hojin-Choi08 <merro3043@gmail.com>
2025-08-08 17:52:56 +0900

    first init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import BasicJsx from "./components/BasicJsx/BasicJsx";
+import BasicProps from "./components/BasicProps/BasicProps";
+import CountState from "./components/BasicState/CountState/CountState";
+
+function App() {
+  return (
+    <>
+      {/* {<BasicReact />} */}
+      {/* <BasicJsx /> */}
+      {/* <BasicProps /> */}
+      <CountState />
+    </>
+  );
+}
+
+export default App;
